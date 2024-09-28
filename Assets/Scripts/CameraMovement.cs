7b0c065900c4dc4f6188a59709969c1cb88f543c
ThamanGurung01 <tgurung519@gmail.com>
2024-09-28 07:21:22 +0545

    movement updates

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,14 +6,25 @@ public class CameraMovement : MonoBehaviour
 {
     public GameObject player;
     // Start is called before the first frame update
+    private float offsetX;
+    private float offsetY = -2f;
+    private float offsetZ = -10f;
+
+    private float cameraLeftLimit;
+    private float cameraInitial;
     void Start()
     {
-        
+        cameraLeftLimit=player.transform.position.x;
+        cameraInitial = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x,player.transform.position.y, -10);
-    }
+        if (cameraLeftLimit<player.transform.position.x&& cameraInitial<player.transform.position.x)
+            {
+            transform.position = new Vector3((player.transform.position.x - offsetX),transform.position.y , offsetZ);
+            }
+        transform.position = new Vector3(transform.position.x,(player.transform.position.y - offsetY), offsetZ);
+        }
 }
